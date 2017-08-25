0ebf959060c79bb3dcd93daeecaf659605f5675e
Yuvaraj <shivarajnaidu@gmail.com>
2017-08-25 11:36:44 +0530

    Moved Db Logic To Seperate Class

--- index.php
+++ index.php
@@ -1,57 +1,24 @@
 <?php
-include_once("xlsxwriter.class.php");
-ini_set('display_errors', 0);
-ini_set('log_errors', 1);
+require_once("db-class.php");
+require_once("xlsxwriter.class.php");
+ini_set("display_errors", 1);
+ini_set("log_errors", 1);
 error_reporting(E_ALL & ~E_NOTICE);
 
 
 function getData() {
-	$servername = "localhost";
-	$username = "username";
-	$password = "password";
-	$dbname = "dbname";
- 	$data = array();
-
-// Create connection
-	$conn = mysqli_connect($servername, $username, $password, $dbname);
-// Check connection
-	if (!$conn) {
-		die("Connection failed: " . mysqli_connect_error());
-	}
-
+	$db = new MY_SQLDB();
 	$sql = "SELECT * FROM mailid";
-	$result = mysqli_query($conn, $sql);
-
-
-    function mk_headers($val) {
-    	return $val->name;
-    }
-
-    $sheet_titles = array_map("mk_headers", mysqli_fetch_fields($result));
- 	array_push($data, $sheet_titles);
-
-	if (mysqli_num_rows($result) > 0) {
-    // output data of each row
-		while($row = mysqli_fetch_array($result, MYSQLI_NUM)) {
-			 array_push($data, $row);
-		}
-
-	} else {
-		echo "0 results";
-	}
-
-	mysqli_free_result($result);
-	mysqli_close($conn);
+	$rows = $db->get_rows($sql);
+	$sheet_titles = $db->get_column_names();
+	$data = array_merge(array(), $rows);
+	array_unshift($data , $sheet_titles);
+	$db->close_connection();
 	return $data;
 }
 
-// print_r(getData());
-
-
-
-
 $data = getData();
-$filename = "example.xlsx";
+$filename = "data.xlsx";
 
 $writer = new XLSXWriter();
 $writer->writeSheet($data);
@@ -63,8 +30,7 @@ header("Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetm
 header('Content-Transfer-Encoding: binary');
 header('Cache-Control: must-revalidate');
 header('Pragma: public');
-    readfile($filename);
-// exit(0);
-
+readfile($filename);
+exit(0);
 
 ?>
